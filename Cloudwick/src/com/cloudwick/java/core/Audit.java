38183c507f9d686f0e1dce306f83fef089779a9a
Harshal Jagtap <hdj3@njit.edu>
2015-09-26 12:01:36 -0700

    Added Audit

---
+++ Cloudwick/src/com/cloudwick/java/core/Audit.java
@@ -0,0 +1,11 @@
+package com.cloudwick.java.core;
+
+import java.lang.annotation.*;
+
+@Target(value=ElementType.METHOD)
+@Retention(value=RetentionPolicy.RUNTIME)
+
+public @interface Audit {
+
+	
+}
